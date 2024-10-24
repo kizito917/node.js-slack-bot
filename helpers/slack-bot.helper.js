ab92fd35fb6aea91bfb800e90e2dc934e429af15
kizito917 <johnkingsley917@gmail.com>
2024-10-24 13:32:26 +0200

    Message delivery function created for reusability.

--- helpers/slack-bot.helper.js
+++ helpers/slack-bot.helper.js
@@ -1,115 +1,4 @@
 const { App, ExpressReceiver } = require('@slack/bolt');
-//     await ack();
-
-//     try {
-//         // Call views.open with the built-in client
-//         const result = await client.views.open({
-//             // Pass a valid trigger_id within 3 seconds of receiving it
-//             trigger_id: body.trigger_id,
-//             // View payload
-//             view: {
-//                 type: 'modal',
-//                 // View identifier
-//                 callback_id: 'view_1',
-//                 title: {
-//                     type: 'plain_text',
-//                     text: 'Modal title'
-//                 },
-//                 blocks: [
-//                     {
-//                         type: 'section',
-//                         text: {
-//                         type: 'mrkdwn',
-//                         text: 'Welcome to a modal with _blocks_'
-//                         },
-//                         accessory: {
-//                         type: 'button',
-//                         text: {
-//                             type: 'plain_text',
-//                             text: 'Click me!'
-//                         },
-//                         action_id: 'button_abc'
-//                         }
-//                     },
-//                     {
-//                         type: 'input',
-//                         block_id: 'input_c',
-//                         label: {
-//                         type: 'plain_text',
-//                         text: 'What are your hopes and dreams?'
-//                         },
-//                         element: {
-//                         type: 'plain_text_input',
-//                         action_id: 'dreamy_input',
-//                         multiline: true
-//                         }
-//                     },
-//                     {
-//                         "type": "actions",
-//                         "block_id": "actions1",
-//                         "elements": [
-//                             {
-//                             "type": "static_select",
-//                             "placeholder":{
-//                                 "type": "plain_text",
-//                                 "text": "Which witch is the witchiest witch?"
-//                             },
-//                             "action_id": "select_2",
-//                             "options": [
-//                                 {
-//                                 "text": {
-//                                     "type": "plain_text",
-//                                     "text": "Matilda"
-//                                 },
-//                                 "value": "matilda"
-//                                 },
-//                                 {
-//                                 "text": {
-//                                     "type": "plain_text",
-//                                     "text": "Glinda"
-//                                 },
-//                                 "value": "glinda"
-//                                 },
-//                                 {
-//                                 "text": {
-//                                     "type": "plain_text",
-//                                     "text": "Granny Weatherwax"
-//                                 },
-//                                 "value": "grannyWeatherwax"
-//                                 },
-//                                 {
-//                                 "text": {
-//                                     "type": "plain_text",
-//                                     "text": "Hermione"
-//                                 },
-//                                 "value": "hermione"
-//                                 }
-//                             ]
-//                             },
-//                             {
-//                             "type": "button",
-//                             "text": {
-//                                 "type": "plain_text",
-//                                 "text": "Cancel"
-//                             },
-//                             "value": "cancel",
-//                             "action_id": "button_1"
-//                             }
-//                         ]
-//                     }
-//                 ],
-//                 submit: {
-//                     type: 'plain_text',
-//                     text: 'Submit'
-//                 }
-//             }
-//         });
-//         console.log(result)
-//     }
-//     catch (error) {
-//         console.log(error)
-//     }
-// });
 
 const receiver = new ExpressReceiver({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -122,7 +11,88 @@ const slackApp = new App({
     // appToken: process.env.APP_TOKEN
 });
 
+const deliverMessageToChannel = async (channel, payload) => {
+    try {
+        await slackApp.client.chat.postMessage({
+            channel: channel,
+            text: payload.description,
+            blocks: [
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "plain_text",
+                            "text": payload.notificationType,
+                            "emoji": true
+                        }
+                    ]
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": payload.title,
+                        "emoji": true
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": payload.description,
+                        "emoji": true
+                    }
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "*Time of Failure:*"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": payload.failureDate,
+                        }
+                    ]
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": payload.impactDescription
+                    }
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "<https://savannahwebgen.tech/|Powered by savannah webgen>."
+                        }
+                    ]
+                }
+            ]
+        });
+
+        return {
+            status: 200,
+            message: 'Message delivered successfully to slack channel'
+        }
+    } catch (err) {
+        console.log(err);
+        return {
+            status: 500,
+            message: 'Unable to deliver message to slack channel'
+        }
+    }
+}
+
 module.exports = {
     receiver,
     slackApp,
+    deliverMessageToChannel
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require("dotenv").config();
 
-const { receiver, slackApp } = require('./helpers/slack-bot.helper');
+const { receiver, deliverMessageToChannel } = require('./helpers/slack-bot.helper');
 
 // Express app
 const app = express();
@@ -13,71 +13,15 @@ app.use('/slack/events', receiver.router);
 // Express route to trigger sending a Slack message
 app.get('/send', async (req, res) => {
     try {
-        await slackApp.client.chat.postMessage({
-            channel: '#automation',
-            text: 'The SPA webgen function failed to generate website for Lubego Tech. This is caused by the functions inability to process retrieveal of information from the database.',
-            blocks: [
-                {
-                    "type": "context",
-                    "elements": [
-                        {
-                            "type": "plain_text",
-                            "text": ":round_pushpin: Incident: Identified",
-                            "emoji": true
-                        }
-                    ]
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "SPA webgen function failure",
-                        "emoji": true
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "The SPA webgen function failed to generate website for Lubego Tech. This is caused by the functions inability to process retrieveal of information from the database.",
-                        "emoji": true
-                    }
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": "*Time of Failure:*"
-                        },
-                        {
-                            "type": "mrkdwn",
-                            "text": "*23/09/2024 18:08*"
-                        }
-                    ]
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Impact*: During this time, websites generation might not be successful when attempting to generate website for different domains."
-                    }
-                },
-                {
-                    "type": "context",
-                    "elements": [
-                        {
-                            "type": "mrkdwn",
-                            "text": "<https://savannahwebgen.tech/|Powered by savannah webgen>."
-                        }
-                    ]
-                }
-            ]
-        });
-        res.send('Status sent to Slack!');
+        const payload = {
+            description: 'The SPA webgen function failed to generate website for Lubego Tech. This is caused by the functions inability to process retrieveal of information from the database.',
+            notificationType: ':round_pushpin: Incident: Identified',
+            title: 'SPA webgen function failure',
+            failureDate: `*${new Date().toLocaleDateString()}*`,
+            impactDescription: '*Impact*: During this time, websites generation might not be successful when attempting to generate website for different domains.'
+        }
+        const { status, message } = await deliverMessageToChannel('#automation', payload);
+        res.status(status).json(message);
     } catch (error) {
         console.error('Error sending message:', error);
         res.status(500).send('Failed to send status');
